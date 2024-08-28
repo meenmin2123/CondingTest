15b593d848c24b1965d9bc2e3caf058008a10449
meenmin2123 <min1016alsrud@gmail.com>
2024-08-28 23:39:16 +0900

    DAY2 COMMIT

--- SQL/MySQL/day2.sql
+++ SQL/MySQL/day2.sql
@@ -2,3 +2,10 @@ SELECT F.FLAVOR AS FLAVOR FROM FIRST_HALF AS F
 INNER JOIN ICECREAM_INFO AS I ON F.FLAVOR = I.FLAVOR 
 WHERE TOTAL_ORDER > 3000 AND I.INGREDIENT_TYPE = 'fruit_based'
 ORDER BY TOTAL_ORDER DESC
+
+---------------------------------------------------------------------
+
+SELECT user_id,product_id  FROM ONLINE_SALE
+GROUP BY user_id,product_id
+HAVING COUNT(product_id) > 1
+ORDER BY user_id, product_id DESC
